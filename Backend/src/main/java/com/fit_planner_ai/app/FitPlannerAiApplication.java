81695d8499e7b8f3b6ed1bb07e527fc5947aa2b9
roberto <roberto.sodini1@gmail.com>
2025-09-21 15:03:55 +0200

    feat Directory
    Creazione del backend in Spring Boot e frontend in React Js

---
+++ Backend/src/main/java/com/fit_planner_ai/app/FitPlannerAiApplication.java
@@ -0,0 +1,13 @@
+package com.fit_planner_ai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPlannerAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitPlannerAiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fit_planner_ai/app/FitPlannerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit_planner_ai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPlannerAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
